fc9526cc16c049b3101e02388a90bd978666c5ec
= <=>
2021-10-01 00:12:49 -0300

    first commit

--- main.py
+++ main.py
@@ -30,26 +30,42 @@ while (iniciar == 1):
 
   ESCOLHA UMA OPÇÃO: ''')
 
-  if (opcao == 1 or opcao == "1"):
-    os.system("cls")
+  opcao = opcao.strip()
+
+  if (opcao == "1"):
+    os.system('cls' if os.name == 'nt' else 'clear')
     repete = 1
     while (repete == 1):
       try:
-        nome = input("Digite o nome da pessoa: ")
-        idade = int(input("Digite a idade da pessoa: "))
-        repete = 0
+        nome = str(input('''(Digite "menu" para voltar ao menu principal)
+Digite o nome da pessoa: '''))
+        if (nome.upper().replace(" ", "") == "MENU"):
+          repete = 0
+          input("Pressione ENTER para continuar")
+          os.system('cls' if os.name == 'nt' else 'clear')
+            
+        else:  
+          idade = int(input('''Digite a idade da pessoa: '''))
+          if idade < 0:
+            print("ERRO - A idade deve ser a partir de 0")
+            input("Pressione ENTER para continuar")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            repete = 1
+          else:
+            repete = 0
+            adicionar_pessoa(nome, idade)
+            print(f"ADICIONADO: {lista_pessoas[-1]}")
+            input("Pressione ENTER para continuar")
+            os.system('cls' if os.name == 'nt' else 'clear')
+      
       except ValueError:
         print("ERRO - A idade precisa ser um número inteiro")
+        input("Pressione ENTER para continuar")
+        os.system('cls' if os.name == 'nt' else 'clear')
         repete = 1
-
-    adicionar_pessoa(nome, idade)
-    print(f"ADICIONADO: {lista_pessoas[-1]}")
-
-    os.system("pause")
-    os.system("cls")
     
-  elif (opcao == 2 or opcao == "2"):
-    os.system("cls")
+  elif (opcao == "2"):
+    os.system('cls' if os.name == 'nt' else 'clear')
     repete = 1
     while (repete == 1):
       for n in enumerate(lista_pessoas):
@@ -60,50 +76,51 @@ while (iniciar == 1):
 Digite o número referente à pessoa ou "menu" para voltar ao menu principal: ''')
         codigo_escolhido = int(codigo_escolhido)
         if (codigo_escolhido < len(lista_pessoas) and codigo_escolhido >= 0):
-          print(f"Usuário {lista_pessoas[codigo_escolhido]} apagado")
+          print(f"REMOVIDO: Usuário: {lista_pessoas[codigo_escolhido]}")
           del lista_pessoas[codigo_escolhido]
           repete = 0
-          os.system("pause")
-          os.system("cls")
+          input("Pressione ENTER para continuar")
+          os.system('cls' if os.name == 'nt' else 'clear')
 
         else:
           print("ERRO - Insira um número inteiro existente na lista")
-          os.system("pause")
-          os.system("cls")
+          input("Pressione ENTER para continuar")
+          os.system('cls' if os.name == 'nt' else 'clear')
           repete = 1
 
       except ValueError:
-        codigo_escolhido = codigo_escolhido.upper()
+        codigo_escolhido = codigo_escolhido.upper().replace(" ", "")
         if (codigo_escolhido == "MENU"):
           repete = 0
-          os.system("cls")
+          input("Pressione ENTER para continuar")
+          os.system('cls' if os.name == 'nt' else 'clear')
 
         else:
           print("ERRO - Insira um número inteiro existente na lista")
-          os.system("pause")
-          os.system("cls")
+          input("Pressione ENTER para continuar")
+          os.system('cls' if os.name == 'nt' else 'clear')
           repete = 1
 
-  elif (opcao == 3 or opcao == "3"):
-    os.system("cls")
+  elif (opcao == "3"):
+    os.system('cls' if os.name == 'nt' else 'clear')
     lista_pessoas_nome = sorted(lista_pessoas, key = Pessoa.get_nome)
     for n in lista_pessoas_nome:
       print(n)
-    os.system("pause")
-    os.system("cls")
+    input("Pressione ENTER para continuar")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-  elif (opcao == 4 or opcao == "4"):
-    os.system("cls")
+  elif (opcao == "4"):
+    os.system('cls' if os.name == 'nt' else 'clear')
     lista_pessoas_idade = sorted(lista_pessoas, key = Pessoa.get_idade)
     for n in lista_pessoas_idade:
       print(n)
-    os.system("pause")
-    os.system("cls")
+    input("Pressione ENTER para continuar")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-  elif (opcao == 5 or opcao == "5"):
+  elif (opcao == "5"):
     iniciar = 0
 
   else:
     print("ERRO - Insira um número de 1 a 5")
-    os.system("pause")
-    os.system("cls")
+    input("Pressione ENTER para continuar")
+    os.system('cls' if os.name == 'nt' else 'clear')
